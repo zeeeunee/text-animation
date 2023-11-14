621ad3b9580e94c30f7466f2079d55fe6b6183a6
zeeeunee <wldmstv@gmail.com>
2023-11-14 10:39:28 +0900

    tab메뉴 기능 연결

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-splitText('h1');
+const main = document.querySelector('main');
+const btns = main.querySelectorAll('li');
+const boxs = main.querySelectorAll('article');
+
+// btns.forEach((btn, idx) => {
+// 	btn.addEventListener('click', () => {
+// 		btns.forEach((btn) => btn.classList.remove('on'));
+// 		btns[idx].classList.add('on');
+
+// 		boxs.forEach((btn) => btn.classList.remove('on'));
+// 		boxs[idx].classList.add('on');
+// 	});
+// });
+
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', () => {
+		activation(btns, idx);
+		activation(boxs, idx);
+	});
+});
+
+function activation(arr, idx) {
+	arr.forEach((el) => el.classList.remove('on'));
+	arr[idx].classList.add('on');
+}
 
 function splitText(el) {
 	const dom = document.querySelector(el);
